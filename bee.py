a02a9b8646e2177b620083b63343afbca8e685d8
Jiayuan Shi <1305384305@qq.com>
2024-03-22 19:00:32 +0800

    Update bee.py

--- bee.py
+++ bee.py
@@ -5,11 +5,28 @@ import os
 import subprocess
 import psutil
 import pygame
+import requests
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# 全局变量，记录上次请求执行的时间
+last_request_time = 0
+# 设置请求之间的最小时间间隔（单位：秒）
+request_interval = 3600  # 1小时
+
+def request_pushplus():
+    global last_request_time
+    current_time = time.time()
+    if current_time - last_request_time >= request_interval:
+        token = 'bca1d81e1df647769c17fc3a5f012d0f' #在pushplus网站中可以找到
+        title= '电脑断电！' #改成你要的标题内容
+        content ='实验室的电脑' #改成你要的正文内容
+        url = 'http://www.pushplus.plus/send?token='+token+'&title='+title+'&content='+content
+        requests.get(url)
+        last_request_time = current_time
 
 def is_power_disconnected():
     try:
@@ -51,7 +68,7 @@ def set_max_volume_and_beep():
             os.system("powershell (new-object -com wscript.shell).SendKeys([char]176)")
             # 播放蜂鸣声
             pygame.mixer.init()
-            pygame.mixer.music.load("D:\\python\\bee\\dg.mp3")
+            pygame.mixer.music.load("E:\\github_res\\Scare-the-thief\\dg.mp3")
             pygame.mixer.music.set_volume(1.0)
             pygame.mixer.music.play()
             while pygame.mixer.music.get_busy():
@@ -75,6 +92,7 @@ if __name__ == "__main__":
         try:
             if is_power_disconnected():
                 logging.info("检测到电源被拔掉，正在处理...")
+                request_pushplus()
                 set_max_volume_and_beep()
         except Exception as e:
             logging.error(f"执行过程中出现错误: {e}")
